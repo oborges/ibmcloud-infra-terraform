f514da9b22fc2af1a33fdbc1becacfd44bb030a9
Olavo Borges <olavoborges@MacBook-Pro-de-Olavo.local>
2025-07-23 12:50:38 -0300

    fix variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,23 +1,27 @@
 ###############
 #  Variables  #
 ###############
-variable "vpc_name" {
-  description = "Name of the VPC."
-  type        = string
-}
 
 variable "region" {
-  description = "IBM Cloud region (e.g. br-sao, us-south)."
+  description = "IBM Cloud region (default: us-south)."
   type        = string
+  default     = "us-south"
 }
 
 variable "zones" {
   description = "List of availability zones inside the region."
   type        = list(string)
+  default     = ["us-south-1", "us-south-2", "us-south-3"]
+
   validation {
     condition     = length(var.zones) >= 2
-    error_message = "Provide at least two zones for HA."
+    error_message = "Provide at least two zones for high availability."
   }
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC."
+  type        = string
 }
 
 variable "resource_group" {
@@ -30,6 +34,7 @@ variable "vpc_cidr" {
   description = "Base CIDR block for the VPC (must be /16 or /17)."
   type        = string
   default     = "10.10.0.0/16"
+
   validation {
     condition     = cidrsubnet(var.vpc_cidr, 8, 0) != ""
     error_message = "vpc_cidr must be a valid IPv4 CIDR (e.g. 10.10.0.0/16)."
