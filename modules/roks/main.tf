94219c4a5c758f9da0faa0f5ac829a9fb73ef6d0
Olavo Borges <olavoborges@MacBook-Pro-de-Olavo.local>
2025-07-29 16:39:32 -0300

    fixed main.tf

--- modules/roks/main.tf
+++ modules/roks/main.tf
@@ -1,19 +1,17 @@
 ############################################
-# Terraform & required providers           #
+# Terraform & required provider
 ############################################
 terraform {
-  required_version = ">= 1.5"
-
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = ">= 1.12.0"
+      version = ">= 1.60.0"
     }
   }
 }
 
 ############################################
-# Resolve VPC and subnets                  #
+# Resolve VPC and subnets
 ############################################
 data "ibm_is_vpc" "target" {
   name = var.vpc_name
@@ -24,18 +22,19 @@ data "ibm_is_subnet" "zone" {
   identifier = var.subnet_ids[each.value]
 }
 
+############################################
+# Local helper values
+############################################
 locals {
   resource_group_id = data.ibm_is_vpc.target.resource_group
-}
 
-########################################################
-# Optional Standard Cloud Object Storage
-########################################################
-# Decide the target resource‑group
-locals {
+  # Which resource‑group to use for COS?
   cos_rg = var.cos_resource_group_id != "" ? var.cos_resource_group_id : local.resource_group_id
 }
 
+############################################
+# Optional Standard Cloud Object Storage
+############################################
 resource "ibm_resource_instance" "cos" {
   count             = var.create_cos && var.cos_instance_crn == "" ? 1 : 0
   name              = var.cos_name
@@ -46,13 +45,13 @@ resource "ibm_resource_instance" "cos" {
   tags              = var.tags
 }
 
-# Pick the CRN: existing value, or new COS
 locals {
-  effective_cos_crn = var.cos_instance_crn != "" ? var.cos_instance_crn : ibm_resource_instance.cos[0].crn
-
+  # Final CRN that will be fed into the ROKS API
+  effective_cos_crn = var.cos_instance_crn != "" ? var.cos_instance_crn : try(ibm_resource_instance.cos[0].crn, "")
+}
 
 ############################################
-# ROKS VPC cluster                         #
+# ROKS VPC cluster
 ############################################
 resource "ibm_container_vpc_cluster" "this" {
   name              = var.cluster_name
@@ -61,10 +60,9 @@ resource "ibm_container_vpc_cluster" "this" {
   flavor            = var.worker_flavor
   worker_count      = var.workers_per_zone
   resource_group_id = local.resource_group_id
+  entitlement       = "cloud_pak"
+
   cos_instance_crn  = local.effective_cos_crn
-  # Make sure cluster waits until COS exists
-  depends_on        = [ibm_resource_instance.cos]
-  tags              = var.tags
 
   dynamic "zones" {
     for_each = var.zones
@@ -73,5 +71,10 @@ resource "ibm_container_vpc_cluster" "this" {
       subnet_id = data.ibm_is_subnet.zone[zones.value].id
     }
   }
+
+  tags = var.tags
+
+  # Ensure COS is created first if we opted in
+  depends_on = [ibm_resource_instance.cos]
 }
 
